26484bf5e74abb887761bc2a13a18f4cc898ec86
nat3 <billing.nate@gmx.de>
2024-07-24 21:41:14 +0200

    added base files

---
+++ src/example.zig
@@ -0,0 +1,21 @@
+const std = @import("std");
+const dbs = @import("sqlite3-zig");
+pub fn main() !void {
+    var dbhandle: ?*dbs.sqlite3 = undefined;
+    defer {
+        const close_res = dbs.sqlite3_close(dbhandle);
+        if (close_res != dbs.SQLITE_OK) {
+            std.debug.print("failed to close db\n", .{});
+        } else {
+            std.debug.print("successfully closed db\n", .{});
+        }
+    }
+
+    const db_res = dbs.sqlite3_open(":memory:", &dbhandle);
+    if (db_res != dbs.SQLITE_OK) {
+        std.debug.print("sqlite error: {}\n", .{db_res});
+        return error.FAILED_TO_OPEN_SQLITE_DB;
+    } else {
+        std.debug.print("opened in memory sqlite3 database, returned SQLITE_OK\n", .{});
+    }
+}
---
+++ src/sqlite.zig
@@ -0,0 +1,4 @@
+pub usingnamespace @cImport({
+    @cInclude("sqlite3.h");
+    @cInclude("sqlite3ext.h");
+});
